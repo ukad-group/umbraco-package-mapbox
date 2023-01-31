d312588a67e8879f74d9e78f2bfa324e2fdaf43f
vladyslav-nosov-ukad <88329010+vladyslav-nosov-ukad@users.noreply.github.com>
2023-01-31 22:01:32 +0200

    update rendering methods

--- UkadGroup.UmbracoPackageMapbox.Core/Extensions/MapboxExtensions.cs
+++ UkadGroup.UmbracoPackageMapbox.Core/Extensions/MapboxExtensions.cs
@@ -8,9 +8,14 @@ namespace UkadGroup.UmbracoPackageMapbox.Core.Extensions
 {
     public static class MapboxExtensions
     {
-        public static IHtmlContent MapboxRendering(this MapboxMarkerMapModel model)
+        public static IHtmlContent RenderMarkerMap(this MapboxMarkerMapModel model)
         {
-            return new HtmlString(@$"<div data-mapboxopenstreetmap='{JsonConvert.SerializeObject(model)}' data-mapboxopenstreetmapdefaultconfig='{JsonConvert.SerializeObject(model.Configuration)}' style='width: 100%; height: 400px;'></div>");
+            return new HtmlString(@$"<div data-mapboxmarkermap='{JsonConvert.SerializeObject(model)}' data-mapboxmarkermapdefaultconfig='{JsonConvert.SerializeObject(model.Configuration)}' style='width: 100%; height: 400px;'></div>");
+        }
+
+        public static IHtmlContent RenderRasterLayerMap(this MapboxRasterLayerMapModel model)
+        {
+            return new HtmlString(@$"<div data-mapboxrasterlayermap='{JsonConvert.SerializeObject(model)}' data-mapboxrasterlayermapdefaultconfig='{JsonConvert.SerializeObject(model.Configuration)}' style='width: 100%; height: 400px;'></div>");
         }
 
         public static IHtmlContent MapboxScripts(this IHtmlHelper htmlHelper)
@@ -20,12 +25,58 @@ namespace UkadGroup.UmbracoPackageMapbox.Core.Extensions
 <script src=""https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.js""></script>
 <script>
 
-let elements = document.querySelectorAll('[data-mapboxopenstreetmap]');
-        
-elements.forEach(elem => {
+let markermapelements = document.querySelectorAll('[data-mapboxmarkermap]'); 
+markermapelements.forEach(elem => {
             
-let data = JSON.parse(elem.getAttribute('data-mapboxopenstreetmap'));
-let config = JSON.parse(elem.getAttribute('data-mapboxopenstreetmapdefaultconfig'));
+let markermapdata = JSON.parse(elem.getAttribute('data-mapboxmarkermap'));
+let markermapconfig = JSON.parse(elem.getAttribute('data-mapboxmarkermapdefaultconfig'));
+
+if(!markermapconfig.accessToken)
+{
+const error = document.createElement('p');
+error.innerText = 'No Mapbox access token set, Maps Editor cannot load.';
+elem.appendChild(error);
+return;
+}
+
+const southWest = new mapboxgl.LngLat(
+    markermapdata.boundingBox.southWestCorner.longitude,
+    markermapdata.boundingBox.southWestCorner.latitude
+);
+const northEast = new mapboxgl.LngLat(
+    markermapdata.boundingBox.northEastCorner.longitude,
+    markermapdata.boundingBox.northEastCorner.latitude
+);
+const boundingBox = new mapboxgl.LngLatBounds(southWest, northEast);
+
+const map = new mapboxgl.Map({
+	accessToken: markermapconfig.accessToken,
+    container: elem,
+    animate: false,
+    style: ""mapbox://styles/mapbox/streets-v12"",
+    center: [
+        markermapdata.marker?.longitude ?? boundingBox.getCenter().lng,
+        markermapdata.marker?.latitude ?? boundingBox.getCenter().lat,
+    ],
+    zoom: markermapdata.zoom,
+}).fitBounds(boundingBox);
+
+if (markermapdata.marker && markermapdata.marker.longitude && markermapdata.marker.latitude) {
+    const marker = new mapboxgl.Marker({ draggable: false })
+        .setLngLat([
+            markermapdata.marker.longitude,
+            markermapdata.marker.latitude,
+        ])
+        .addTo(map);
+}
+    });
+
+
+let rasterlayermapelements = document.querySelectorAll('[data-mapboxrasterlayermap]');
+rasterlayermapelements.forEach(elem => {
+            
+let data = JSON.parse(elem.getAttribute('data-mapboxrasterlayermap'));
+let config = JSON.parse(elem.getAttribute('data-mapboxrasterlayermapdefaultconfig'));
 
 if(!config.accessToken)
 {
@@ -51,21 +102,52 @@ const map = new mapboxgl.Map({
     animate: false,
     style: ""mapbox://styles/mapbox/streets-v12"",
     center: [
-        data.marker?.longitude ?? boundingBox.getCenter().lng,
-        data.marker?.latitude ?? boundingBox.getCenter().lat,
+        boundingBox.getCenter().lng,
+        boundingBox.getCenter().lat,
     ],
     zoom: data.zoom,
 }).fitBounds(boundingBox);
 
-if (data.marker && data.marker.longitude && data.marker.latitude) {
-    const marker = new mapboxgl.Marker({ draggable: false })
-        .setLngLat([
-            data.marker.longitude,
-            data.marker.latitude,
-        ])
-        .addTo(map);
-}
+
+map.on(""load"", () => 
+		{
+
+if (data.image
+&& data.topLeftPoint?.longitude && data.topLeftPoint?.latitude
+&& data.topRightPoint?.longitude && data.topRightPoint?.latitude
+&& data.bottomLeftPoint?.longitude && data.bottomLeftPoint?.latitude
+&& data.bottomRightPoint?.longitude && data.bottomRightPoint?.latitude) 
+			{
+
+                         map.addSource(""image"", {
+                         type: ""image"",
+                         url: data.image,
+                         coordinates: [
+                            [data.topLeftPoint.longitude, data.topLeftPoint.latitude],
+                            [data.topRightPoint.longitude, data.topRightPoint.latitude],
+                            [data.bottomRightPoint.longitude, data.bottomRightPoint.latitude],
+                            [data.bottomLeftPoint.longitude, data.bottomLeftPoint.latitude]]
+                        });
+
+                        map.addLayer({
+                            id: ""image"",
+                            type: ""raster"",
+                            source: ""image"",
+                            paint: {
+                                ""raster-fade-duration"": 0,
+                            },
+                        });
+
+			}
+
+
+		});
+
+
+
     });
+
+
 </script>");
         }
     }
