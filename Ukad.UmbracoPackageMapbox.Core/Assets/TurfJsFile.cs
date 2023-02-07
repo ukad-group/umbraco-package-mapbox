4824851feac9d12c51ced1263011022bd8c26a12
duyun-yevhen <85081556+duyun-yevhen@users.noreply.github.com>
2023-02-07 16:55:07 +0200

    add turf js

---
+++ Ukad.UmbracoPackageMapbox.Core/Assets/TurfJsFile.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Cms.Core.WebAssets;
+
+namespace Ukad.UmbracoPackageMapbox.Core.Assets
+{
+    internal class TurfJsFile : JavaScriptFile
+    {
+        public TurfJsFile()
+            : base("/App_Plugins/Ukad.UmbracoPackageMapbox/lib/Turf.js/turf.min.js")
+        { }
+    }
+}
